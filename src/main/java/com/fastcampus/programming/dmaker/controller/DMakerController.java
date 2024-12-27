720591d5617188d1f29ca58002c09d1452f280ac
BOOK2-PRO\csy30 <csy30423@gmail.com>
2024-12-27 15:28:45 +0900

    2024/12/26 h2 데이터베이스 구현

--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,5 +1,7 @@
 package com.fastcampus.programming.dmaker.controller;
 
+import com.fastcampus.programming.dmaker.service.DMakerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,18 +9,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 @RestController // DMakerController를  RestController 타입의 빈으로 등록
+@RequiredArgsConstructor
 public class DMakerController {
-
-    private static final Logger log = LoggerFactory.getLogger(DMakerController.class);
+    private final DMakerService dMakerService;
 
     @GetMapping("/developers")
     public List<String> getAllDevelopers() {
         log.info("GET /developers HTTP/1.1");
 
         return Arrays.asList("snow", "elsa", "Olaf");
+    }
+
+    @GetMapping("/create-developers")
+    public List<String> createDevelopers() {
+        log.info("GET /create-developers HTTP/1.1");
+
+        dMakerService.createDeveloper();
+
+        return Collections.singletonList("Olaf");
     }
 }
